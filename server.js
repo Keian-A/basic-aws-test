da22de567a790692668679594e38b1dd6cc8957a
Keian <KeianAnthony1@gmail.com>
2022-01-31 11:35:56 -0800

    adds status

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Welcome to your basic server!')
+  res.status(200).send('Welcome to your basic server!')
 });
 
 app.listen(PORT, () => {
